301d8c46a4729decd7625595b40f2cfe3133b22c
David Kotlirevsky <david.kotlirevsky@gmail.com>
2023-01-20 13:46:55 -0300

    First test asynchronous

--- tests/commons_test.py
+++ tests/commons_test.py
@@ -1,4 +1,11 @@
 from fastapi.testclient import TestClient
+from httpx import AsyncClient
+
 from main import app
 
 client = TestClient(app)
+
+async def async_get_call(url):
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.get(url)
+    return response
--- tests/main/main_e2e_test.py
+++ tests/main/main_e2e_test.py
@@ -1,7 +1,17 @@
-from tests.commons_test import client
+from tests.commons_test import client, async_get_call
+import pytest
 
-
+expected_result = {"application": "FASTAPI POC"}
+"""Not async call"""
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"application": "FASTAPI POC"}
+    assert response.json() == expected_result
+
+"""async call"""
+@pytest.mark.anyio
+async def test_root():
+    response = await async_get_call('/')
+    assert response.status_code == 200
+    assert response.json() == expected_result
+
